bb38f447b7489656c3a2040ddf1994ddb1efcc98
adhpawal <pawaladhikari@lftechnology.com>
2016-06-24 23:05:15 +0545

    Add Changes to Properties file read

--- src/main/java/utility/ImgFlipUtils.java
+++ src/main/java/utility/ImgFlipUtils.java
@@ -2,13 +2,13 @@ package utility;
 
 import org.funtastic.exception.NotValidException;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
-import com.google.gson.JsonObject;
-
+@Component
 public class ImgFlipUtils {
 
 	@Value("${imgflip.api.endpoint}")
-	private static String IMG_FLIP_REST_ENDPOINT;
+	private static String IMG_FLIP_REST_ENDPOINT = "https://api.imgflip.com/";
 
 	public static String getMeme() throws NotValidException {
 		return RestApiUtils.get(IMG_FLIP_REST_ENDPOINT + "get_memes");
--- src/main/java/utility/RestApiUtils.java
+++ src/main/java/utility/RestApiUtils.java
@@ -1,15 +1,11 @@
 package utility;
 
-import org.funtastic.enums.GiphyType;
 import org.funtastic.exception.NotValidException;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
-
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 public class RestApiUtils {
 
@@ -44,7 +40,7 @@ public class RestApiUtils {
 		String result = null;
 		try {
 			HttpHeaders headers = new HttpHeaders();
-			headers.set("User-Agent", "ozilla/4.0");
+			headers.set("User-Agent", "Mozilla/4.0");
 			HttpEntity<String> response = restTemplate.exchange(uri, HttpMethod.GET, new HttpEntity(headers),
 					String.class);
 			result = response.getBody();
