94f0bbcb0624bb6453a337d6578f89edf1de34fe
adhpawal <pawaladhikari@lftechnology.com>
2016-06-24 21:11:02 +0545

    Add Controller Layer to fetch Giphy API's

--- src/main/java/org/funtastic/controller/CommentOptionController.java
+++ src/main/java/org/funtastic/controller/CommentOptionController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import utility.GiphyUtils;
 import utility.ImgFlipUtils;
@@ -23,11 +24,14 @@ import utility.ImgFlipUtils;
 public class CommentOptionController {
 
 	@RequestMapping(value = "/meme", method = RequestMethod.GET)
+	@ResponseBody
 	public String getMeme() throws NotValidException {
+		System.out.println("paal");
 		return ImgFlipUtils.getMeme();
 	}
 
 	@RequestMapping(value = "/gif/{type}/{searchTerm}", method = RequestMethod.GET)
+	@ResponseBody
 	public String get(@PathVariable("type") String type, @PathVariable("searchTerm") String term)
 			throws NotValidException {
 		return GiphyUtils.get(GiphyType.getEnum(type), term);
