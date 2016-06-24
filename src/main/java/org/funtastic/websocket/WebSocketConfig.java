0862ee7a15931f7a7dd74964106dd22bb97030b2
adhpawal <pawaladhikari@lftechnology.com>
2016-06-25 00:52:37 +0545

    Add WebConfig for Websocket

---
+++ src/main/java/org/funtastic/websocket/WebSocketConfig.java
@@ -0,0 +1,30 @@
+package org.funtastic.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+/**
+ * {@link WebSocketConfig}
+ * 
+ * @author adhpawal
+ * @see http://www.baeldung.com/websockets-spring
+ */
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/chat").withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		config.enableSimpleBroker("/topic");
+		config.setApplicationDestinationPrefixes("/app");
+	}
+}
