57662f8f04babc52772da94d72bdf4a15f6b09fd
coolnimesh43 <coolnimesh43@gmail.com>
2016-06-24 20:07:39 +0545

    added getter and setter

--- src/main/java/org/funtastic/entity/User.java
+++ src/main/java/org/funtastic/entity/User.java
@@ -121,6 +121,14 @@ public class User extends AbstractEntity{
 		this.genderType = genderType;
 	}
 
+	public List<Group> getGroups() {
+		return groups;
+	}
+
+	public void setGroups(List<Group> groups) {
+		this.groups = groups;
+	}
+
 	@Override
 	public String toString() {
 		return "User [firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + ", password=" + password
