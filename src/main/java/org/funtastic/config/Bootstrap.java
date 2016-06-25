72fb6c59ceb131b971b7da444f5b34d2a14d50b5
Srijan Bajracharya <srijanbajracharya@lftechnology.com>
2016-06-25 07:17:17 +0545

    changes in bootstrap.java

--- src/main/java/org/funtastic/config/Bootstrap.java
+++ src/main/java/org/funtastic/config/Bootstrap.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.funtastic.entity.Group;
 import org.funtastic.entity.User;
+import org.funtastic.enums.Gender;
 import org.funtastic.service.GroupService;
 import org.funtastic.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,41 +18,42 @@ import org.springframework.stereotype.Component;
 @Component
 public class Bootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
-	private static final Logger LOG = LogManager.getLogger(Bootstrap.class);
-	@Autowired
-	private UserService userService;
+    private static final Logger LOG = LogManager.getLogger(Bootstrap.class);
+    @Autowired
+    private UserService userService;
 
-	@Autowired
-	private GroupService groupService;
+    @Autowired
+    private GroupService groupService;
 
-	private User getUsers() {
-		User user = new User();
-		user.setFirstName("First Name");
-		user.setLastName("Last Name");
-		user.setEmail("coolnimesh43@gmail.com");
-		user.setPassword("nimesh");
-		return user;
-	}
+    private User getUsers() {
+        User user = new User();
+        user.setFirstName("First Name");
+        user.setLastName("Last Name");
+        user.setEmail("coolnimesh43@gmail.com");
+        user.setPassword("nimesh");
+        user.setGenderType(Gender.MALE);
+        return user;
+    }
 
-	private Group getGroup() {
-		Group group = new Group("WhiteBeard Pirates");
-		return group;
-	}
+    private Group getGroup() {
+        Group group = new Group("WhiteBeard Pirates");
+        return group;
+    }
 
-	@Override
-	public void onApplicationEvent(ContextRefreshedEvent arg0) {
-		LOG.debug("Bootstrap#creating user");
-		if (this.userService.getAll().isEmpty() && this.groupService.getAll().isEmpty()) {
-			List<Group> groups = new ArrayList<>();
-			User usr = getUsers();
-			Group g = getGroup();
-			Group g1 = new Group("Black Beard Pirates");
-			groups.add(g);
-			groups.add(g1);
-			usr.setGroups(groups);
-			usr = this.userService.save(usr);
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent arg0) {
+        LOG.debug("Bootstrap#creating user");
+        if (this.userService.getAll().isEmpty() && this.groupService.getAll().isEmpty()) {
+            List<Group> groups = new ArrayList<>();
+            User usr = getUsers();
+            Group g = getGroup();
+            Group g1 = new Group("Black Beard Pirates");
+            groups.add(g);
+            groups.add(g1);
+            usr.setGroups(groups);
+            usr = this.userService.save(usr);
 
-		}
-	}
+        }
+    }
 
 }
